3aa4a7d7ff6250926247aa712347a77e79ab4c65
Alvey 2.0 <Alvey 2.0@THESHEIKAHTOWER>
2017-11-17 18:42:56 -0700

    Add project files.

---
+++ Assignment3CS3750/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment3CS3750.MvcApplication" Language="C#" %>
---
+++ Assignment3CS3750/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment3CS3750.Startup))]
+namespace Assignment3CS3750
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
