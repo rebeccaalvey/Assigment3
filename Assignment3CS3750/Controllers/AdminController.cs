213f5a3f3ccf1f9229cc6763671c68d8bdc9e9fb
mrplayer47 <nosmrplayer@gmail.com>
2017-11-20 18:57:22 -0700

    admin controller

---
+++ Assignment3CS3750/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3CS3750.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ViewResult Login()
+        {
+
+
+            return View();
+        }
+    }
+}
--- Assignment3CS3750/Global.asax.cs
+++ Assignment3CS3750/Global.asax.cs
@@ -10,6 +10,23 @@ namespace Assignment3CS3750
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Admin",
+                "Admin/{action}/{id}",
+                new { controller = "Admin", action = "Login", id = "" }
+                );
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = "" }
+            );
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
