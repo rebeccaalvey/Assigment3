4df14d5fbadae877a545658c983a78ff618c3393
mikigiffin <32532287+mikigiffin@users.noreply.github.com>
2017-11-27 20:32:58 -0700

    added new controller

---
+++ Assignment3CS3750/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment3CS3750.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Student()
+        {
+            return View();
+        }
+    }
+}
